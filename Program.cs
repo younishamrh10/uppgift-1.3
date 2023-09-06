3635563d33f393236968c3899c4e99731b3fac06
yoha06001 <yoha06001@E-PW0234KT>
2023-09-06 11:01:35 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace uppgift4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   /\\");
+            Console.WriteLine("  /  \\");
+            Console.WriteLine(" / \"\" \\");
+            Console.WriteLine("/______\\");
+            Console.ReadKey();
+
+        }
+    }
+}
